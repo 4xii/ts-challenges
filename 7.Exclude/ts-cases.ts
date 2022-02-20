5997d0029d1ecac4a1703c3165e60211253f8753
4xii <975795875@qq.com>
2022-02-20 23:33:28 +0800

    feat:1-7 e xexclude

---
+++ 7.Exclude/ts-cases.ts
@@ -0,0 +1,11 @@
+
+
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+    Expect<Equal<MyExclude<"a" | "b" | "c", "a">, Exclude<"a" | "b" | "c", "a">>>,
+    Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, Exclude<"a" | "b" | "c", "a" | "b">>>,
+    Expect<Equal<MyExclude<string | number | (() => void), Function>, Exclude<string | number | (() => void), Function>>>,
+]
+type MyExclude1<T, U> = T extends U ? 2 : T;
+type a = MyExclude1<"a" | "b" | "c", "a">
